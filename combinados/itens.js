0404dad437b2ac958b6e57528d16d59dfc318659
nocturne7br <celsotec@gmail.com>
2026-05-23 09:06:44 -0300

    itens 0.6

--- combinados/itens.js
+++ combinados/itens.js
@@ -18,7 +18,7 @@
         thumb: "img/hikari-28und.jpg?w=600&q=80"
       },
       {
-        id: 0, category: "Novidades",
+        id: 3, category: "Novidades",
         title: "Combinado Shiro 28 und",
         description: "Composição do combinado: 10 Hots Filadélfia Crocantes, 10 Uramakis Terrine Especial Roasted, 4 Hossomakis Filadélfia, 4 Uramakis Filadélfia",
         price: "R$ 0",
@@ -27,7 +27,7 @@
         thumb: "img/shiro-28und.jpg?w=600&q=80"
       },
       {
-        id: 1, category: "Novidades",
+        id: 4, category: "Novidades",
         title: "Combinado Taizen 49 und",
         description: "Composição do combinado: 12 Hots Filadélfia Crocantes, 6 Hossomakis Filadélfia, 6 Uramakis Filadélfia, 4 Gunkans Salmão Maracujá, 3 Gunkans Salmão, 4 Niguiris Salmão Especial Roasted, 4 Niguiris Salmão, 5 Sashimis Peixe Branco com Tougarashi, 5 Sashimis Salmão",
         price: "R$ 0",
@@ -35,7 +35,7 @@
         image: "img/taizen-49und.jpg?w=1200&q=80",
         thumb: "img/taizen-49und.jpg?w=600&q=80"
       },      {
-        id: 0, category: "Novidades",
+        id: 5, category: "Novidades",
         title: "Temaki Hot",
         description: "Um cone irresistível recheado com arroz de sushi temperado, cream cheese cremoso e cubos de salmão fresco. Empanado em farinha panko e frito na hora. Com teriyaki por cima.",
         price: "R$ 0",
@@ -45,7 +45,7 @@
       },
       
       {
-        id: 2, category: "Promoções",
+        id: 6, category: "Promoções",
         title: "Combinado Ishigaki 9 und",
         description: "Composição: - 2 Hots Crocantes com Crisps de Batata-Doce, - 2 Hots Crocantes com Crisps de Couve, - 2 Hossomakis com Terrine Skin e Cream Cheese, - 2 Hots com Cream Cheese e Salmão, - 1 Niguiri Skin Especial",
         price: "R$ 0",
@@ -54,7 +54,7 @@
         thumb: "img/ishigaki-9und.jpg?w=600&q=80"
       },
             {
-        id: 2, category: "Promoções",
+        id: 7, category: "Promoções",
         title: "Compre 1 e leve 2! Temaki Salmão Grande + Temaki Skin Grande",
         description: "Composição: Temaki de Salmão com cream cheese, cebolinha e gergelim, e o irresistível Temaki Skin com cream cheese e gergelim ",
         price: "R$ 0",
@@ -63,7 +63,7 @@
         thumb: "img/temaki-salmao-temaki-skin.jpg?w=600&q=80"
       },
       {
-        id: 3, category: "Promoções",
+        id: 8, category: "Promoções",
         title: "Hot Filadélfia Crocante 12 und",
         description: "Alga por fora, recheado com salmão, arroz japonês e cream cheese. Empanado e frito na hora, ficando super crocante",
         price: "R$ 0",
@@ -72,7 +72,7 @@
         thumb: "img/hot-12und.jpg?w=600&q=80"
       },
       {
-        id: 4, category: "Pokes",
+        id: 9, category: "Pokes",
         title: "Poke Domó Salmão com Ceviche",
         description: "*POKE 500g* - 200g Arroz Japonês, - 100g  Cubos de Salmão Fresco, - 30g   Ceviche de Peixe Branco,- 40g   Manga Doce, - 40g   Abacate Cremoso, - 40g   Tomatinhos, - 40g   Sunomono, - 10g   Milho Desidratado. Com toque de mel, gergelim e cebolinha.",
         description2: "*POKE 350g* - 140g Arroz Japonês, - 70g   Cubos de Salmão Fresco, - 21g   Ceviche de Peixe Branco, - 28g  Manga Doce, - 28g  Abacate Cremoso, - 28g  Tomatinhos, - 28g  Sunomono, - 7g  Milho Desidratado. Com toque de mel, gergelim e cebolinha.",
@@ -82,7 +82,7 @@
         thumb: "img/poke-salmao.jpg?w=600&q=80"
       },
       {
-        id: 5, category: "Pokes",
+        id: 10, category: "Pokes",
         title: "Poke Domó Camarão",
         description: "*Poke 500g* -200g Arroz Japonês, - 100g Camarão Crocante, - 30g Ceviche de Peixe Branco, - 40g Manga Doce, - 40g Abacate Cremoso, - 40g Tomate Cereja, - 40g Sunomono, - 10g Milho Desidratado. Com toque de mel, gergelim e cebolinha.",
         description2: "*Poke 350g* -140g Arroz Japonês, - 70g  Camarão Crocante, - 21g  Ceviche de Peixe Branco, - 28g  Tomatinhos, - 28g  Sunomono, - 28g  Manga Doce, - 28g  Abacate Cremoso, - 7g  Milho Desidratado. Com toque de mel, gergelim e cebolinha.",
@@ -92,7 +92,7 @@
         thumb: "img/poke-camarao.jpg?w=600&q=80"
       },
       {
-        id: 6, category: "Pokes",
+        id: 11, category: "Pokes",
         title: "Poke Shimeji",
         description:  "*Poke 500g* - 200g Arroz Japonês, - 100g Frango Grelhado com Lemon Pepper, - 30g Shimeji Puxado na Manteiga, - 40g Manga Doce, - 40g Abacate Cremoso, - 40g Tomatinhos, - 40g Sunomono, - 10g Milho Desidratado. Com toque de mel, gergelim e cebolinha",
         description2: "*Poke 350g* - 140g Arroz Japonês, - 70g  Frango Grelhado com Lemon Pepper, - 21g  Shimeji Puxado na Manteiga, - 28g  Tomate Cereja, - 28g  Sunomono, - 28g  Manga, - 28g  Abacate, - 7g   Milho Desidratado Com toque de mel, gergelim e cebolinha.",
@@ -102,7 +102,7 @@
         thumb: "img/poke-shimeji.jpg?w=600&q=80"
       },
             {
-        id: 7, category: "Pokes",
+        id: 12, category: "Pokes",
         title: "Poke Domó Frango com Shimeji",
         description:  "*Poke 500g* - 200g Arroz Japonês, - 130g Shimeji Puxado na Manteiga, - 40g Manga Doce, - 40g Abacate Cremoso, - 40g Tomatinhos, - 40g Sunomono, - 10g Milho Desidratado. Com toque de mel, gergelim e cebolinha",
         description2: "*Poke 350g* - 140g Arroz Japonês, - 91g  Shimeji Puxado na Manteiga, - 28g  Manga Doce, - 28g  Abacate Cremoso,- 28g  Tomatinhos, - 28g  Sunomono, - 7g  Milho Desidratado. Com toque de mel, gergelim e cebolinha.",
@@ -112,12 +112,12 @@
         thumb: "img/poke-frago-com-shimeji.jpg?w=600&q=80"
       },
       {
-        id: 7, category: "Bebidas",
-        title: "Cocktail da Casa",
-        description: "Composição exclusiva do nosso bartender: gin artesanal macerado com pétalas de rosa e lavanda, água tônica premium, redução de framboesa, ramos de alecrim flambados e esfera de gelo artesanal. Servido em taça de cristal.",
-        price: "R$ 52",
+        id: 13, category: "Temakis",
+        title: "Combo Temaki Skin Grande + Coca + 4 Gunkan Goiabada",
+        description: "• 1 Temaki com Pele de Salmão Frita, Cream Cheese, Cebolinha e Gergelim. • 1 Coca Lata 350ml. • 4 Gunkan Romeu e Julieta (Goiabada e Cream Cheese com Raspas de Limão).",
+        price: "R$ 0",
         color: "#1a2030",
-        image: "https://images.unsplash.com/photo-1551024709-8f23befc6f87?w=1200&q=80",
-        thumb: "https://images.unsplash.com/photo-1551024709-8f23befc6f87?w=600&q=80"
+        image: "img/temaki-skin-coca-romeu-julieta-4und.jpg?w=1200&q=80",
+        thumb: "img/temaki-skin-coca-romeu-julieta-4und.jpg?w=600&q=80"
       }
     ];
